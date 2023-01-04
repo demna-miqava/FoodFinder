2771f567c4c625dd35c3b7a3d12f34cb65169c66
demna-miqava <demnamikava11@gmail.com>
2023-01-04 22:43:44 +0400

    add safe area view in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {NativeBaseProvider, View} from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 import RNBootSplash from 'react-native-bootsplash';
 import MainScreen from '@screens/MainScreen';
 import {theme} from '@app/theme';
+import {SafeAreaView} from 'react-native';
 const App = () => {
   React.useEffect(() => {
     setTimeout(() => {
@@ -11,9 +12,13 @@ const App = () => {
   }, []);
   return (
     <NativeBaseProvider theme={theme}>
-      <View>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: 'teal',
+        }}>
         <MainScreen />
-      </View>
+      </SafeAreaView>
     </NativeBaseProvider>
   );
 };
