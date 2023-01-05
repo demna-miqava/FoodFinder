7ef9eae73a22b5655a6f3fb45e44dd316ebaa5d4
demna-miqava <demnamikava11@gmail.com>
2023-01-05 14:33:52 +0400

    add makeRequestWrapper container component

---
+++ src/containers/MakeRequestWrapper.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface Props {
+  isFetching: boolean;
+  isError: boolean;
+  isEmpty: boolean;
+  data: unknown[];
+  renderLoader: () => React.ReactElement;
+  renderData: (items: any[]) => React.ReactElement;
+}
+
+export const MakeRequestWrapper = ({
+  isFetching,
+  isError,
+  isEmpty,
+  data,
+  renderLoader,
+  renderData,
+}: Props) => {
+  if (isFetching) {
+    return renderLoader();
+  }
+  // add error and empty state handling
+  if (isError || isEmpty) {
+    return null;
+  }
+  return renderData(data);
+};
