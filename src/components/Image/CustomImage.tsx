f876838572a12105e9c20de355c1400dd6ee3587
demna-miqava <demnamikava11@gmail.com>
2023-01-05 09:02:21 +0400

    add custom image

---
+++ src/components/Image/CustomImage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Image as NativeImage, ImageResizeMode} from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+interface CustomImageProps {
+  source: any;
+  style: any;
+  resizeMode?: ImageResizeMode;
+}
+
+export const Image = ({source, style, resizeMode}: CustomImageProps) => {
+  return (source as any).uri ? (
+    <FastImage
+      style={{...(style as any)}}
+      source={{
+        uri: source.uri,
+        priority: FastImage.priority.normal,
+        cache: FastImage.cacheControl.immutable,
+      }}
+      resizeMode={(resizeMode as any) || FastImage.resizeMode.cover}
+    />
+  ) : (
+    <NativeImage source={source} resizeMode={resizeMode} style={style} />
+  );
+};
