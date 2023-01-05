3d883ae288e529eca71888a292537705393be257
demna-miqava <demnamikava11@gmail.com>
2023-01-05 14:37:14 +0400

    add restaurant cards dummy data with loading state

--- src/features/restaurants/screens/RestaurantsScreen.tsx
+++ src/features/restaurants/screens/RestaurantsScreen.tsx
@@ -1,7 +1,11 @@
 import {FlatList, useTheme, View} from 'native-base';
-import React from 'react';
+import React, {useState} from 'react';
 import {SearchBar} from '@components/SearchBar';
-import RestaurantsInfoCard from '../components/RestaurantInfoCard';
+import {RestaurantsInfoCard} from '../components/RestaurantInfoCard';
+import {CardSkeleton} from '@components/Skeleton';
+import {MakeRequestWrapper} from '@app/containers';
+import {RestaurantCardType} from '@app/types';
+import {ThemeType} from '@app/theme';
 
 const restaurant = {
   name: 'Some restaurant',
@@ -16,26 +20,56 @@ const restaurant = {
   isClosedTemporarily: false,
 };
 
+// move this to separate file
+const dummyLoadingData = [{id: '1'}, {id: '2'}, {id: '3'}];
+
 const ResutaurantsScreen = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [data, setData] = useState<RestaurantCardType[]>([
+    restaurant,
+    restaurant,
+    restaurant,
+    restaurant,
+  ]);
   const theme = useTheme();
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
       <View style={stylesBuilder(theme).searchContainer}>
-        <SearchBar label={'Search restaurants'} />
+        <SearchBar label={'Search Restaurants'} />
       </View>
       <View style={stylesBuilder(theme).textContainer}>
-        <FlatList
-          data={[
-            {name: 1},
-            {name: 2},
-            {name: 3},
-            {name: 4},
-            {name: 5},
-            {name: 6},
-            {name: 7},
-          ]}
-          renderItem={() => <RestaurantsInfoCard restaurant={restaurant} />}
-          keyExtractor={item => item.name}
+        <MakeRequestWrapper
+          data={data}
+          isFetching={isLoading}
+          isEmpty={data?.length === 0}
+          renderLoader={() => {
+            return (
+              <FlatList
+                data={dummyLoadingData}
+                renderItem={() => <CardSkeleton />}
+                keyExtractor={item => item.id}
+              />
+            );
+          }}
+          isError={isError}
+          renderData={(items: RestaurantCardType[]) => {
+            return (
+              <FlatList
+                data={items}
+                renderItem={() => (
+                  <RestaurantsInfoCard restaurant={restaurant} />
+                )}
+                keyExtractor={item => item.name}
+              />
+            );
+          }}
         />
       </View>
     </>
@@ -44,7 +78,7 @@ const ResutaurantsScreen = () => {
 
 export default ResutaurantsScreen;
 
-const stylesBuilder = theme => ({
+const stylesBuilder = (theme: ThemeType) => ({
   searchContainer: {
     padding: theme.space[3],
   },
