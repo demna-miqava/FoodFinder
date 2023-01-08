9a15e11072921379702e8ba6af4ed1558563b7ae
demna-miqava <demnamikava11@gmail.com>
2023-01-08 19:49:49 +0400

    add temporary statusbar config

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import RNBootSplash from 'react-native-bootsplash';
 import {theme} from '@app/theme';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import ResutaurantsScreen from '@features/restaurants/screens/RestaurantsScreen';
 import './src/assets/localization/';
@@ -20,6 +20,7 @@ const App = () => {
             flex: 1,
             backgroundColor: 'teal',
           }}>
+          <StatusBar backgroundColor="teal" barStyle="dark-content" />
           <ResutaurantsScreen />
         </SafeAreaView>
       </NativeBaseProvider>
