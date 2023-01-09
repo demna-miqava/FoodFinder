f18a63ede1f62d312493660c6022f9b9242e4b6b
demna-miqava <demnamikava11@gmail.com>
2023-01-09 09:03:11 +0400

    add navigation route alias

--- babel.config.js
+++ babel.config.js
@@ -20,6 +20,7 @@ module.exports = {
           '@features': './src/features',
           '@containers': './src/containers',
           '@helpers': './src/helpers',
+          '@navigations': './src/navigation',
         },
       },
     ],
