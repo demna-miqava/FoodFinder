d9f3223df9270b9f212db2984ede683e811219a7
demna-miqava <demnamikava11@gmail.com>
2023-01-19 17:38:19 +0400

    refactor useNoWifiToast

--- src/hooks/useNoWifiToast.tsx
+++ src/hooks/useNoWifiToast.tsx
@@ -3,29 +3,31 @@ import {useNetInfo} from '@react-native-community/netinfo';
 import {useToast} from 'native-base';
 import {Alert} from '@components/Alert';
 import {ToastIds} from '@constants/';
+import {useTranslation} from 'react-i18next';
 
 const noWifi = ToastIds.NoWifi;
 
-const title = 'Oops, you have no internet access';
-const message = 'Please turn wifi on to see restaurants nearby';
+const title = 'noInternetConnectionTitle';
+const message = 'noInternetConnectionMessage';
 
 export const useNoWifiToast = () => {
   const toast = useToast();
+  const {t} = useTranslation();
   const {isConnected} = useNetInfo();
   const isToastActive = toast.isActive(noWifi);
   useEffect(() => {
     if (isConnected) {
       isToastActive && toast.close(noWifi);
     } else {
-      console.log('is act', isToastActive);
       !isToastActive &&
         toast.show({
           id: noWifi,
           placement: 'top',
           render: () => (
-            <Alert status="error" title={title} message={message} />
+            <Alert status="error" title={t(title)} message={t(message)} />
           ),
         });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isConnected]);
 };
