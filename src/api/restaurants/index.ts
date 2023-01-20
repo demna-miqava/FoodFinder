a2eb59580b11a665927f2d60a1375ec785171f97
demna-miqava <demnamikava11@gmail.com>
2023-01-20 17:21:24 +0400

    create custom hook for getting restaurants

--- src/api/restaurants/index.ts
+++ src/api/restaurants/index.ts
@@ -1,7 +1,21 @@
+import {useQuery} from 'react-query';
 import {mocks} from './mock';
+import {customRestaunrantsInstance} from './restaurants.instance';
+
+export const getRestaurantsNearby = (location: string) => {
+  return customRestaunrantsInstance({
+    url: 'some api address',
+    method: 'get',
+    params: {location},
+  });
+};
 
 export const useGetRestaurants = (location: string) => {
-  //  change with real api req
-  //@ts-ignore
-  return {data: mocks[location]};
+  return useQuery('restaurants', async () => {
+    // replace with this will be in the future
+    // const data = await getRestaurantsNearby(location);
+    // @ts-ignore
+    const data = mocks[location];
+    return data;
+  });
 };
