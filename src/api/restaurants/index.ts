14ab731c63c7bf236e6701d2179a26dc3faa32b3
demna-miqava <demnamikava11@gmail.com>
2023-01-22 19:11:19 +0400

    add useGetLocation

--- src/api/restaurants/index.ts
+++ src/api/restaurants/index.ts
@@ -1,6 +1,7 @@
 import {useQuery} from 'react-query';
-import {mocks} from './mock';
 import {customRestaunrantsInstance} from './restaurants.instance';
+import {mocks, locations} from './mock';
+import {LocationResults} from '@type/';
 
 export const getRestaurantsNearby = (location: string) => {
   return customRestaunrantsInstance({
@@ -10,12 +11,39 @@ export const getRestaurantsNearby = (location: string) => {
   });
 };
 
+export const getLocation = (searchTerm: string) => {
+  // create custom instance for location if needed
+  return customRestaunrantsInstance({
+    url: 'some url',
+    method: 'get',
+    params: {searchTerm},
+  });
+};
+
 export const useGetRestaurants = (location: string) => {
   return useQuery('restaurants', async () => {
-    // replace with this will be in the future
+    // replace with this in the future
     // const data = await getRestaurantsNearby(location);
     // @ts-ignore
     const data = mocks[location];
     return data;
+  });
+};
+
+const transformLocation = (data: LocationResults) => {
+  const [{geometry}] = data.results;
+  const {location} = geometry;
+  const {lat, lng} = location;
+  return `${lat},${lng}`;
+};
+
+export const useGetLocation = (searchTerm: string) => {
+  return useQuery('location', async () => {
+    // replace with this in the future
+    // const data = await getLocation(searchTerm);
+    // @ts-ignore
+    const locationData = locations[searchTerm];
+    const result = transformLocation(locationData);
+    return result;
   });
 };
