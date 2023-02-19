4ddfcf6cdd477291e8dbf3eea821f082b868a4ef
demna-miqava <demnamikava11@gmail.com>
2023-02-19 16:42:46 +0400

    create useFavorite hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export {useNoWifiToast} from './useNoWifiToast';
 export {useDebounce} from './useDebounce';
 export {useAppState} from './useAppState';
 export {useRestaurant} from './useRestaurant';
+export * from './redux';
---
+++ src/hooks/redux/useFavorites.ts
@@ -0,0 +1,35 @@
+import {
+  clearFavorites as clearFavs,
+  injectFavorites as injectFavs,
+  toggleFavorites as toggleFavs,
+  useAppDispatch,
+  useAppSelector,
+} from '@app/redux';
+import {useCallback} from 'react';
+
+export const useFavorites = () => {
+  const dispatch = useAppDispatch();
+  const favorites = useAppSelector(state => state.favorites.favoritePlaces);
+
+  const clearFavorites = useCallback(() => {
+    dispatch(clearFavs());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const injectFavorites = useCallback((data: string[]) => {
+    dispatch(injectFavs(data));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const toggleFavorites = useCallback((data: string) => {
+    dispatch(toggleFavs(data));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    favorites,
+    clearFavorites,
+    injectFavorites,
+    toggleFavorites,
+  };
+};
