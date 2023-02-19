409821b8db054e4a9e2529c566892a7a41eebf7f
demna-miqava <demnamikava11@gmail.com>
2023-02-19 15:51:31 +0400

    add favorites icon

---
+++ src/assets/icons/HeartIcon.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SvgCss} from 'react-native-svg';
+import {SvgIconProps} from 'src/types/svg.types';
+
+const xml = (props: SvgIconProps) => {
+  const {width = 16, fill = 'gray', height = 16} = props;
+  return `
+		<svg
+			width="${width}"
+			height="${height}"
+			viewBox="0 0 12 12"
+			fill="${fill}"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M1.5 6.00031C0.375 4.50031 0.75 2.25031 2.625 1.50031C4.5 0.750305 5.625 2.25031 6 3.00031C6.375 2.25031 7.875 0.750305 9.75 1.50031C11.625 2.25031 11.625 4.50031 10.5 6.00031C9.375 7.50031 6 10.5003 6 10.5003C6 10.5003 2.625 7.50031 1.5 6.00031Z"
+				stroke="${fill}"
+				stroke-width="1"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+     `;
+};
+
+export const HeartIcon = (props: SvgIconProps) => <SvgCss xml={xml(props)} />;
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,4 @@ export {StarIcon} from './StarIcon';
 export {HomeIcon} from './HomeIcon';
 export {MapsIcon} from './MapsIcon';
 export {SettingsIcon} from './SettingsIcon';
+export {HeartIcon} from './HeartIcon';
