c8f37419ac43b526957b7da9a2905f943e060aff
demna-miqava <demnamikava11@gmail.com>
2023-02-19 18:39:02 +0400

    navigate to favorite restaurants screen on heart icon press

--- src/features/restaurants/components/RestaurantSearch.tsx
+++ src/features/restaurants/components/RestaurantSearch.tsx
@@ -4,6 +4,8 @@ import {spaces} from '@theme/index';
 import {SearchBar} from '@components/SearchBar';
 import {useTranslation} from 'react-i18next';
 import {HeartIcon} from '@app/assets/icons';
+import {Pressable} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 type Props = {
   setSearchCity: React.Dispatch<React.SetStateAction<string>>;
@@ -11,12 +13,20 @@ type Props = {
 
 export const RestaurantSearch = ({setSearchCity}: Props) => {
   const {t} = useTranslation();
+  const navigation = useNavigation<any>();
   return (
     <View pb={spaces[3]}>
       <SearchBar
         label={t('searchRestaurnats')}
         onChange={setSearchCity}
-        icon={<HeartIcon width={40} height={20} />}
+        icon={
+          <Pressable
+            onPress={() => {
+              navigation.push('FavouriteRestaurants');
+            }}>
+            <HeartIcon width={40} height={20} />
+          </Pressable>
+        }
       />
     </View>
   );
