eecd0553dcc60c1dda735020e752a88172a7b983
demna-miqava <demnamikava11@gmail.com>
2023-02-19 19:06:48 +0400

    render favorites

--- src/features/restaurants/screens/FavouriteRestaurantsScreen.tsx
+++ src/features/restaurants/screens/FavouriteRestaurantsScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {useFavorites} from '@app/hooks';
+import {RestaurantsList} from '../components';
+import {View} from 'native-base';
 import {Text} from '@components/Typography';
+import {spaces} from '@app/theme';
+import {useTranslation} from 'react-i18next';
 
-type Props = {};
-
-export const FavouriteRestaurantsScreen = (props: Props) => {
+export const FavouriteRestaurantsScreen = () => {
   const {favorites} = useFavorites();
-  return <Text>Favorites</Text>;
+  const {t} = useTranslation();
+  return (
+    <View flex={1} background="bg.primary">
+      <Text mx={spaces[3]} fontSize="body">
+        {t('favorites')}
+      </Text>
+      <RestaurantsList items={favorites} />
+    </View>
+  );
 };
