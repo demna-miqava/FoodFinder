2edd1d4a013e9acb9aa2d5b4aea3f28f2a838638
demna-miqava <demnamikava11@gmail.com>
2023-02-19 18:35:37 +0400

    update search bar to display heart icon

--- src/features/restaurants/components/RestaurantSearch.tsx
+++ src/features/restaurants/components/RestaurantSearch.tsx
@@ -3,6 +3,7 @@ import {View} from 'native-base';
 import {spaces} from '@theme/index';
 import {SearchBar} from '@components/SearchBar';
 import {useTranslation} from 'react-i18next';
+import {HeartIcon} from '@app/assets/icons';
 
 type Props = {
   setSearchCity: React.Dispatch<React.SetStateAction<string>>;
@@ -12,7 +13,11 @@ export const RestaurantSearch = ({setSearchCity}: Props) => {
   const {t} = useTranslation();
   return (
     <View pb={spaces[3]}>
-      <SearchBar label={t('searchRestaurnats')} onChange={setSearchCity} />
+      <SearchBar
+        label={t('searchRestaurnats')}
+        onChange={setSearchCity}
+        icon={<HeartIcon width={40} height={20} />}
+      />
     </View>
   );
 };
