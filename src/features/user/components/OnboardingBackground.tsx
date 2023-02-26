eef406770943abc3380cfccaab9d89ccb56b70ad
demna-miqava <demnamikava11@gmail.com>
2023-02-26 17:22:27 +0400

    refactor

--- src/features/user/components/OnboardingBackground.tsx
+++ src/features/user/components/OnboardingBackground.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {ImageBackground} from 'react-native';
-import {View} from 'native-base';
+import {ImageBackground, StyleSheet} from 'react-native';
 
 interface Props {
   children: React.ReactElement;
@@ -10,10 +9,19 @@ const imageSrc = require('../../../assets/images/home_bg.jpg');
 
 export const OnboardingBackground = ({children}: Props) => {
   return (
-    <View flex={1}>
-      <ImageBackground source={imageSrc} resizeMode="cover" style={{flex: 1}}>
-        {children}
-      </ImageBackground>
-    </View>
+    <ImageBackground
+      source={imageSrc}
+      resizeMode="cover"
+      style={styles.container}>
+      {children}
+    </ImageBackground>
   );
 };
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
