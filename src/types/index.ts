1c2b2083d8a0f73ede5c40526bf2131838f22c1e
demna-miqava <demnamikava11@gmail.com>
2023-02-26 15:26:24 +0400

    add user interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export type {SvgIconProps} from './svg.types';
 export type {RestaurantCardType} from './restaurantCard.types';
 export type {LocationResults} from './location.types';
 export type {UseQueryOptionsType} from './useQueryOptions.types';
+export type {User} from './user.types';
