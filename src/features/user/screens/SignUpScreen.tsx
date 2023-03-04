8feba0fa6d2a6bffec76916633171a99c2069d62
demna-miqava <demnamikava11@gmail.com>
2023-03-04 18:22:15 +0400

    use custom btn and update label translations

--- src/features/user/screens/SignUpScreen.tsx
+++ src/features/user/screens/SignUpScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Text} from '@components/Typography';
 import {OnboardingBackground} from '../components/OnboardingBackground';
 import {View} from 'native-base';
 import {useSignUpForm} from '../hooks';
-import {StyleSheet, TouchableOpacity} from 'react-native';
 import {CustomInput} from '../components/CustomInput';
 import {spaces} from '@app/theme';
 import {User} from '@app/types';
+import {Button} from '@components/Button';
+import {useTranslation} from 'react-i18next';
 
 export const SignUpScreen = () => {
-  const {control, handleSubmit, errors} = useSignUpForm();
+  const {t} = useTranslation();
+  const {control, handleSubmit, errors, isValid} = useSignUpForm();
   const onSubmit = (data: User) => {
     console.log('data', data);
   };
@@ -19,44 +20,47 @@ export const SignUpScreen = () => {
       <View w="100%" px={spaces[4]}>
         <CustomInput
           name="firstName"
-          label="First name"
+          label={t('sign_up_form_first_name')}
           control={control}
           errors={errors}
         />
         <CustomInput
           name="lastName"
-          label="Last name"
+          label={t('sign_up_form_last_name')}
           control={control}
           errors={errors}
         />
         <CustomInput
           name="email"
-          label="Email"
+          label={t('sign_up_form_email')}
           control={control}
           errors={errors}
         />
         <CustomInput
           name="password"
-          label="Password"
-          type="password"
+          label={t('sign_up_form_password')}
+          type="text"
           control={control}
           errors={errors}
         />
-        <TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.btn}>
-          <Text>Sign up</Text>
-        </TouchableOpacity>
+        <Button
+          text={t('sign_up_form_btn_text')}
+          onPress={handleSubmit(onSubmit)}
+          styles={btnStyles}
+          disabled={!isValid}
+          isLoading={false}
+          isLoadingText={t('sign_up_btn_loading') || ''}
+        />
       </View>
     </OnboardingBackground>
   );
 };
 
-const styles = StyleSheet.create({
-  btn: {
-    alignSelf: 'center',
-    paddingHorizontal: 20,
-    paddingVertical: 5,
-    // temporary
-    backgroundColor: '#2182BD',
-    borderRadius: 10,
-  },
-});
+const btnStyles = {
+  alignSelf: 'center',
+  px: 25,
+  py: 1,
+  backgroundColor: '#000',
+  borderRadius: 10,
+  mt: 10,
+};
