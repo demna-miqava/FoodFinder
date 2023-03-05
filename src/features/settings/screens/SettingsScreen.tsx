e2af3a4e64792c15f113c69cdc37128d9fc7c3c7
demna-miqava <demnamikava11@gmail.com>
2023-03-05 20:29:50 +0400

    render user email in profile

--- src/features/settings/screens/SettingsScreen.tsx
+++ src/features/settings/screens/SettingsScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Avatar, View} from 'native-base';
+import {Avatar, View, VStack} from 'native-base';
 import {SettingsList} from '../components/';
 import {HeartIcon, LogoutIcon} from '@app/assets/icons';
 import {spaces} from '@app/theme';
 import {SpacerComponent} from '@components/Spacer';
+import {Text} from '@components/Typography';
 import {useUser} from '@hooks/';
 import {getUserInitials} from '@app/helpers';
 import {User} from '@app/types';
@@ -22,20 +23,23 @@ const SettingsListItems = [
 
 export const SettingsScreen = () => {
   const {user} = useUser();
-  const {firstName, lastName} = user as User;
+  const {firstName, lastName, email} = user as User;
 
   return (
     <View flex={1} px={spaces[3]} py={spaces[1]}>
-      <Avatar
-        alignSelf="center"
-        w="120"
-        h="120"
-        bgColor="brand.primary"
-        _text={{
-          fontSize: 'body',
-        }}>
-        {getUserInitials(firstName, lastName)}
-      </Avatar>
+      <VStack space={2} alignSelf="center">
+        <Avatar
+          alignSelf="center"
+          w="120"
+          h="120"
+          bgColor="brand.primary"
+          _text={{
+            fontSize: 'body',
+          }}>
+          {getUserInitials(firstName, lastName)}
+        </Avatar>
+        <Text>{email}</Text>
+      </VStack>
       <SpacerComponent height="40px" />
       <SettingsList items={SettingsListItems} />
     </View>
