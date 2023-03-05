c4142b12502213d03eca47b7f4359f4c18723a73
demna-miqava <demnamikava11@gmail.com>
2023-03-05 20:07:22 +0400

    render favorites and logout list items in Settings Screen

--- src/features/settings/screens/SettingsScreen.tsx
+++ src/features/settings/screens/SettingsScreen.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
-import {Text} from '@components/Typography';
+import {View} from 'native-base';
+import {SettingsList} from '../components/';
+import {HeartIcon, LogoutIcon} from '@app/assets/icons';
+import {spaces} from '@app/theme';
+
+const SettingsListItems = [
+  {
+    text: 'favorites',
+    icon: <HeartIcon fill="red" bgFill="red" />,
+    secondaryText: 'view_your_favorites',
+  },
+  {
+    text: 'logout',
+    icon: <LogoutIcon />,
+  },
+];
 
 export const SettingsScreen = () => {
-  return <Text>Settings123</Text>;
+  return (
+    <View flex={1} px={spaces[3]} py={2}>
+      <SettingsList items={SettingsListItems} />
+    </View>
+  );
 };
