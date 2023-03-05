0b37023ddd60a95fa2b73b24b70b2b989f57396f
demna-miqava <demnamikava11@gmail.com>
2023-03-05 11:13:36 +0400

    add sign in screen

--- src/features/user/screens/SignInScreen.tsx
+++ src/features/user/screens/SignInScreen.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import {Text} from '@components/Typography';
 import {OnboardingBackground} from '../components/OnboardingBackground';
+import {useSignInForm} from '../hooks/signin';
+import {useTranslation} from 'react-i18next';
+import {CustomInput} from '../components/CustomInput';
+import {AuthButton} from '../components';
 
 export const SignInScreen = () => {
+  const {t} = useTranslation();
+  const {control, errors, isValid, isLoading, onSubmit} = useSignInForm();
   return (
     <OnboardingBackground>
-      <Text>Sign in</Text>
+      <CustomInput
+        name="email"
+        label={t('auth_form_email')}
+        control={control}
+        errors={errors}
+      />
+      <CustomInput
+        name="password"
+        label={t('auth_form_password')}
+        control={control}
+        errors={errors}
+      />
+      <AuthButton
+        text={t('auth_form_sign_in_btn_text')}
+        disabled={!isValid}
+        onPress={onSubmit}
+        isLoading={isLoading}
+        isLoadingText={t('auth_btn_loading') || ''}
+      />
     </OnboardingBackground>
   );
 };
