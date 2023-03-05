06d93bb4ab7a8378facb4462abe403098ec8aa88
demna-miqava <demnamikava11@gmail.com>
2023-03-05 20:26:04 +0400

    render user initials in avatar

--- src/features/settings/screens/SettingsScreen.tsx
+++ src/features/settings/screens/SettingsScreen.tsx
@@ -4,6 +4,9 @@ import {SettingsList} from '../components/';
 import {HeartIcon, LogoutIcon} from '@app/assets/icons';
 import {spaces} from '@app/theme';
 import {SpacerComponent} from '@components/Spacer';
+import {useUser} from '@hooks/';
+import {getUserInitials} from '@app/helpers';
+import {User} from '@app/types';
 
 const SettingsListItems = [
   {
@@ -18,10 +21,20 @@ const SettingsListItems = [
 ];
 
 export const SettingsScreen = () => {
+  const {user} = useUser();
+  const {firstName, lastName} = user as User;
+
   return (
     <View flex={1} px={spaces[3]} py={spaces[1]}>
-      <Avatar alignSelf="center" w="120" h="120" bgColor="brand.primary">
-        DM
+      <Avatar
+        alignSelf="center"
+        w="120"
+        h="120"
+        bgColor="brand.primary"
+        _text={{
+          fontSize: 'body',
+        }}>
+        {getUserInitials(firstName, lastName)}
       </Avatar>
       <SpacerComponent height="40px" />
       <SettingsList items={SettingsListItems} />
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -9,3 +9,8 @@ export const GOOGLE_API_BASE_URL = Config.GOOGLE_API_BASE_URL;
 export const isPlatform = (platform: string) => {
   return Platform.OS === platform;
 };
+
+export const getUserInitials = (firstName: string, lastName: string) => {
+  const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`;
+  return initials.toUpperCase();
+};
