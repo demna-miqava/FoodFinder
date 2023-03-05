ae67a900fba37d122d086452f3b83d614e4e1da3
demna-miqava <demnamikava11@gmail.com>
2023-03-05 12:24:44 +0400

    add content to introduction screen

--- src/features/user/screens/MainScreen.tsx
+++ src/features/user/screens/MainScreen.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {Text} from '@components/Typography';
 import {OnboardingBackground} from '../components/OnboardingBackground';
+import {useTranslation} from 'react-i18next';
+import {HStack} from 'native-base';
+import {OnboardingCard} from '../components';
+import {spaces} from '@app/theme';
 
 export const MainScreen = () => {
+  const {t} = useTranslation();
   return (
     <OnboardingBackground>
-      <Text>Main screen</Text>
+      <Text textAlign="center" fontSize="button" fontWeight="bold">
+        {t('introduction_text')}
+      </Text>
+      <HStack mt={spaces[1]} justifyContent="space-around">
+        <OnboardingCard text={t('sign_in')} navigateTo="SignIn" />
+        <OnboardingCard text={t('sign_up')} navigateTo="SignUp" />
+      </HStack>
     </OnboardingBackground>
   );
 };
