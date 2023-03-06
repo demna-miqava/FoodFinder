d4f6ba87abb3d0022f9b12d798a295c3afe539c1
demna-miqava <demnamikava11@gmail.com>
2023-03-06 16:26:33 +0400

    add toast ids to enum

--- src/constants/toastIds.ts
+++ src/constants/toastIds.ts
@@ -1,3 +1,6 @@
 export enum ToastIds {
   NoWifi = 'no-internet-connection',
+  AddedToFavoritesSuccessFully = 'added-to-favorites-successfully',
+  RemovedFromFavoritesSuccessFully = 'removed-from-favorites-successfully',
+  SignedUpSuccessFully = 'signed-up-successfully',
 }
