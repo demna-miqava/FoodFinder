559b14e41db78d8c35c681ae4234e5e948d3bf3b
demna-miqava <demnamikava11@gmail.com>
2023-03-12 16:03:14 +0400

    change loading logic for restraurants

--- src/features/restaurants/hooks/useRestaurant.ts
+++ src/features/restaurants/hooks/useRestaurant.ts
@@ -3,23 +3,24 @@ import {useGetLocation, useGetRestaurants} from '@api/restaurants';
 import {useDebounce} from '@hooks/';
 
 export const useRestaurant = () => {
+  // add pagination
   const [searchCity, setSearchCity] = useState<string>('');
   const {debouncedValue: debouncedSearchedCity} = useDebounce({
     value: searchCity,
     delay: 1000,
   });
 
-  const {data: locationData, refetch: refetchLocation} = useGetLocation(
-    debouncedSearchedCity as string,
-    {
-      enabled: false,
-    },
-  );
+  const {
+    data: locationData,
+    refetch: refetchLocation,
+    isFetching: isLocationFetching,
+  } = useGetLocation(debouncedSearchedCity as string, {
+    enabled: false,
+  });
 
   const {
     data: restaurantsData,
-    isLoading,
-    isFetching,
+    isFetching: isRestaurantsFetching,
     isError,
     refetch: refetchRestaurants,
   } = useGetRestaurants(locationData as string, {
@@ -43,7 +44,7 @@ export const useRestaurant = () => {
   return {
     setSearchCity,
     restaurantsData,
-    isLoading: isFetching || isLoading,
+    isLoading: isRestaurantsFetching || isLocationFetching,
     isError,
   };
 };
